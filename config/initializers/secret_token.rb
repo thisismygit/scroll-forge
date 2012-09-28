d2988bdc85590676dd7916b98f1ec6f38f410d0a
TheNotary <no@mail.plz>
2012-09-28 10:11:00 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScrollForge::Application.config.secret_token = '087a63c677daa27fc5e81e3458d8c92d56a6113ee982eb033d5b383f0d66c72a3ce7832b4f86972b1702f2d02c1dbc090a9bc12e4cf16a55e6c8997978392c54'
